a99e7c7930780b6878f1f1a805ebcb01f3182e56
shahin mondal <shahinmondal15bd@gmail.com>
2020-08-06 03:27:50 +0300

    all right

---
+++ action/js/custom.js
@@ -0,0 +1,30 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+/*<!--1sites--end-->*/
+/*<!--4sites--start--> */
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+
+/*
+Double exposure is photographic technique that combines 2 different images into a single image. 
+Then I use this technique with canvas blend modes.
+*/
+
+
